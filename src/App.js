fe2c1dae54f280acc03fa68c2eeddd2b086d1e87
Hishaam <hishaamtchirou73@gmail.com>
2022-08-11 12:52:45 +0100

    Ajouts d'éléments 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
-import Banner from './Banner'
+//import './App.css'
 //import Cart from './Cart'
-import ShoppingList from './ShoppingList';
+import Banner from './Banner'
+import Footer from './Footer'
+import logo from './assets/logo.png'
+import ShoppingList from './ShoppingList'
 
 function App() {
   return (
     <div>
-      <Banner />
+      <Banner>
+      <img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+      </Banner>
       {/* <Cart /> */}
       <ShoppingList />
+      <Footer />
     </div>
   );
 }
