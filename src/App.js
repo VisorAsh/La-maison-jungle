065cafd3de549d312b5f762068904ed9ed053cde
Hishaam <hishaamtchirou73@gmail.com>
2022-08-12 15:40:30 +0100

    Ajouts d'éléments 3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 //import './App.css'
-//import Cart from './Cart'
+import Cart from './Cart'
 import Banner from './Banner'
 import Footer from './Footer'
 import logo from './assets/logo.png'
 import ShoppingList from './ShoppingList'
+import './Layout.css'
 
 function App() {
   return (
     <div>
       <Banner>
-      <img src={logo} alt='La maison jungle' className='lmj-logo' />
-				<h1 className='lmj-title'>La maison jungle</h1>
+      <img src={logo} alt='Floral Store' className='lmj-logo' />
+				<h1 className='lmj-title'>Visor Floral Store</h1>
       </Banner>
-      {/* <Cart /> */}
-      <ShoppingList />
+      <div className='lmj-layout-inner'>
+				<Cart />
+				<ShoppingList />
+			</div>
       <Footer />
     </div>
   );
